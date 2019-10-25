fd65738870d8f0a60cc88eca9e94b5ef966e4dd6
htmartin <helen.taylor.martin@gmail.com>
2019-10-25 15:49:12 -0600

    exercises

--- practice.py
+++ practice.py
@@ -1,5 +1,37 @@
 #!/usr/bin/python
 
+
+
+#Assume that two variables, varA and varB, are assigned values, either numbers or strings. 
+#Write a piece of Python code that evaluates varA and varB, and then prints out one of the following messages:
+
+#"string involved" if either varA or varB are strings
+
+#"bigger" if varA is larger than varB
+
+#"equal" if varA is equal to varB
+
+#"smaller" if varA is smaller than varB
+
+varA = 5
+varB= "equal"
+
+if isinstance(varA,str):
+	  print("string involved")
+elif isinstance(varB,str):
+	  print("string involved")
+elif varA > varB:
+	  print("bigger")
+elif varA == varB:
+	  print("equal")
+elif varA < varB:
+	  print("smaller")
+
+
+
+
+
+
 # x,y,z print largest odd number, if nonw odd,print message
 
 x=37
